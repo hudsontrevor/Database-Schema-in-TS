f55955cf2422ac3677d95c3ccb0b431469e1db3f
hudsontrevor <trevorhuds0n175@gmail.com>
2026-09-23 16:18:58 +0300

    DB - first commit

--- tempCodeRunnerFile.ts
+++
@@ -1,3 +0,0 @@
-
-// Users.update_all((records) => { if (records.name == "NJUSH!") { records.name = "RADABROOOO"; return true } return false })
-// Users.update_once((records) => { if (records.name == "NJUSH3") { records.name = "RADABROOOO"; return true } return false })
